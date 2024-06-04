09a3cc2b9db673214b0313a35ff74db2342355fc
Svetlana Riasnova <R_S_U@outlook.com>
2024-06-04 18:58:37 +0300

    added store and project details page, css fixed

---
+++ interno/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <HomePage />
+    <BlogPage />
+    <BlogDetailsPage />
+    <ProjectPage />
+    <ProjectDetailsPage />
+  </div>
+</template>
+
+<script>
+import HomePage from "./components/pages/HomePage.vue";
+import BlogDetailsPage from "./components/pages/BlogDetailsPage.vue";
+import BlogPage from "./components/pages/BlogPage.vue";
+import ProjectPage from "./components/pages/ProjectPage.vue";
+import ProjectDetailsPage from "./components/pages/ProjectDetailsPage.vue";
+
+export default {
+  name: "App",
+  components: {
+    HomePage,
+    BlogPage,
+    BlogDetailsPage,
+    ProjectPage,
+    ProjectDetailsPage,
+  },
+};
+</script>
+
+<style lang="scss"></style>
---
+++ interno/src/components/pages/ProjectDetailsPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="project__details">
+    <HeaderComponent />
+    <BaseBanner :image="`url(${image})`"></BaseBanner>
+    <ProjectDetailsContent />
+    <ProjectDetailsSlider />
+    <FooterComponent />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from "../HeaderComponent.vue";
+import BaseBanner from "../BaseBanner.vue";
+import ProjectDetailsContent from "../ProjectDetailsContent.vue";
+import ProjectDetailsSlider from "../ProjectDetailsSlider.vue";
+import FooterComponent from "../FooterComponent.vue";
+
+export default {
+  neme: "ProjectDetailsPage",
+  components: {
+    HeaderComponent,
+    BaseBanner,
+    ProjectDetailsContent,
+    ProjectDetailsSlider,
+    FooterComponent,
+  },
+  data() {
+    return {
+      image: "ProjectDetailsBanner.png",
+    };
+  },
+};
+</script>
+
+<style lang="scss"></style>
